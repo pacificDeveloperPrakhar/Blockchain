8d6f8dda156ccc0f9cd3993b07ef76bf499a273f
prakhar vishwakarma <prakharvision@gmail.com>
2025-08-26 04:27:37 +0000

    created the block and transaction hash

---
+++ src/blockchain/mod.rs
@@ -0,0 +1,38 @@
+struct Transaction
+{   
+    // id is the transaction hash that is generated from the content of the hash
+    id:String,
+    initiator:String,
+    beneficiar:String,
+    amount:u64,
+    timestamp:u128,
+    signature:String
+}
+// a block struct to define what field should a block should contain
+struct Block
+{
+    sequence:u128,
+    timestamp:u128,
+    current_hash:String,
+    prev_hash:String,
+    // hash of all transaction's hashes within this block
+    merkle_root:String,
+    nonce:String,
+    transactions:Vec<Transaction>,
+    version:u32,
+    difficulty:u64
+}
+
+impl Transaction
+{
+    pub fn verify()->bool
+    {
+        return true,
+    },
+    pub fn new(initiator:String,beneficiar:String,amount:u64)->self
+    {
+      return self {
+
+      }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod consesus;
 pub mod bip;
 pub mod utilities;
+pub mod blockchain;
 fn main() {
 //  let data:String=String::from(r#"{
 //   "from": "Alice",
@@ -10,5 +11,5 @@ fn main() {
 // }"#);
 //   let result:(String,u64)=consesus::proof_of_work::func(&data);
 //   println!("{}-{}",result.0,result.1);
-bip::memonics::gen_memonics();
+
 }
